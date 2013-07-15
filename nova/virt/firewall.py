4d60a01989ff950885cfd10c006d4b67daf9fa4f
Jaroslav Pulchart <jaroslav.pulchart@gooddata.com>
2013-07-15 11:25:08 +0200

    Do not run RPC call for simple db lookup for fixed_ips

--- nova/virt/firewall.py
+++ nova/virt/firewall.py
@@ -405,21 +405,15 @@ class IptablesFirewallDriver(FirewallDriver):
                     fw_rules += [' '.join(args)]
                 else:
                     if rule['grantee_group']:
-                        # FIXME(jkoelker) This needs to be ported up into
-                        #                 the compute manager which already
-                        #                 has access to a nw_api handle,
-                        #                 and should be the only one making
-                        #                 making rpc calls.
-                        nw_api = network.API()
                         for instance in rule['grantee_group']['instances']:
-                            nw_info = nw_api.get_instance_nw_info(ctxt,
-                                                                  instance)
-
-                            ips = [ip['address']
-                                for ip in nw_info.fixed_ips()
-                                    if ip['version'] == version]
-
-                            LOG.debug('ips: %r', ips, instance=instance)
+                            try:
+                                ips = [ip['address']
+                                    for ip in db.fixed_ip_get_by_instance(ctxt, instance['uuid'])]
+                            except:
+                                ips = []
+                            
+                            LOG.debug('ips: %r', ips)
+                            
                             for ip in ips:
                                 subrule = args + ['-s %s' % ip]
                                 fw_rules += [' '.join(subrule)]
